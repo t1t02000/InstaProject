62c2b51da59a37430eebea8af08d59fca35a7f29
Roberto Gutierrez <37553403+t1t02000@users.noreply.github.com>
2025-08-09 23:11:17 -0600

    Update settings.py

--- insta/insta/settings.py
+++ insta/insta/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'insta.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/feed')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,4 +129,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
