d45a65937bb3750bc112203368510d6f06040c1f
Roberto Gutierrez <37553403+t1t02000@users.noreply.github.com>
2025-08-09 22:58:57 -0600

    Update urls.py

--- insta/feed/urls.py
+++ insta/feed/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -22,6 +22,7 @@ urlpatterns = [
 
 
 ]
+
 
 
 
